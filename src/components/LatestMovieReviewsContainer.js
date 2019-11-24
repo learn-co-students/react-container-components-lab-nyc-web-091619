dea12c20ef6d60d47e090cb4e50383b9e5868a0c
github username <github email address>
2019-11-24 01:20:13 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,31 +1,68 @@
+// import React, { Component } from 'react';
+// import 'isomorphic-fetch';
+// import MovieReviews from './MovieReviews'
+
+// const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+// const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+//             + `api-key=${NYT_API_KEY}`;
+
+// // Code LatestMovieReviewsContainer Here
+// class LatestMovieReviewsContainer extends Component {
+
+//     state={
+//         reviews: []
+//     }
+
+//     componentDidMount(){
+//         fetch(URL)
+//         .then(resp=>resp.json())
+//         .then(reviews=> this.setState({reviews: reviews.results}))
+//     }
+
+//     render() {
+//         return (
+//             <div className="latest-movie-reviews">
+//                 <MovieReviews reviews={this.state.reviews} /> 
+//             </div>
+//         );
+//     }
+// }
+
+// export default LatestMovieReviewsContainer;
+  
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
 class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
 
-    state={
-        reviews: []
-    }
+    this.state = {
+      reviews: []
+    };
+  }
 
-    componentDidMount(){
-        fetch(URL)
-        .then(resp=>resp.json())
-        .then(reviews=> this.setState({reviews: reviews.results}))
-    }
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(response => this.setState({ reviews: response.results }));
+  }
 
-    render() {
-        return (
-            <div className="latest-movie-reviews">
-                <MovieReviews reviews={this.state.reviews} /> 
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
 }
+
 
 export default LatestMovieReviewsContainer;
